ee668d4b914fc5fec83713638e73ee49278cc00a
GreedoRen <greedo.it@gmail.com>
2019-05-06 17:42:04 +0300

    Create component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-function App() {
-	return <div className='App' />;
+import Home from './components/Home';
+import Post from './components/Post';
+
+export class App extends Component {
+	render() {
+		return (
+			<div className='container'>
+				<Router>
+					<Switch>
+						<Route exact path='/' component={Home} />
+						<Route exact path='/post/:postId' component={Post} />
+					</Switch>
+				</Router>
+			</div>
+		);
+	}
 }
 
 export default App;
